34a03f7899f92df0915403e16dd0b470c552efe1
jimmy <rootkaliente@gmail.com>
2023-11-07 17:11:42 -0500

    [Jimmy] agregar comentarios

--- src/app/main/center/login/login.component.ts
+++ src/app/main/center/login/login.component.ts
@@ -1,136 +1,135 @@
-import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { first, takeUntil } from 'rxjs/operators';
-import { CoreConfigService } from '../../../../@core/services/config.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { AuthenticationService } from '../../../auth/service/authentication.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Validators, FormBuilder, FormGroup} from '@angular/forms';
+import {first, takeUntil} from 'rxjs/operators';
+import {CoreConfigService} from '../../../../@core/services/config.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subject} from 'rxjs';
+import {AuthenticationService} from '../../../auth/service';
+
+/*
+* IFIS
+* credit
+* ESta pantalla sirve para iniciar sesion en la aplicacion
+* Rutas:
+* `${environment.apiUrl}/central/auth/login/`
+* `${environment.apiUrl}/central/usuarios/create/`,
+* `${environment.apiUrl}/central/auth/loginFacebook/`
+*/
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  //  Public
-  public coreConfig: any;
-  public loginForm: FormGroup;
-  public loading = false;
-  public submitted = false;
-  public returnUrl: string;
-  public error = '';
-  public passwordTextType: boolean;
-  public startDateOptions = {
-    altInput: true,
-    mode: 'single',
-    altInputClass: 'form-control flat-picker flatpickr-input invoice-edit-input',
-    enableTime: true
-  };
+export class LoginComponent implements OnInit, OnDestroy {
+    //  Public
+    public coreConfig: any;
+    public loginForm: FormGroup;
+    public loading = false;
+    public submitted = false;
+    public returnUrl: string;
+    public error = '';
+    public passwordTextType: boolean;
 
-  // Private
-  private _unsubscribeAll: Subject<any>;
+    // Private
+    private _unsubscribeAll: Subject<any>;
 
-  /**
-   * Constructor
-   *
-   * @param {CoreConfigService} _coreConfigService
-   */
-  constructor(
-    private _coreConfigService: CoreConfigService,
-    private _formBuilder: FormBuilder,
-    private _route: ActivatedRoute,
-    private _router: Router,
-    private _authenticationService: AuthenticationService
-  ) {
-    this._unsubscribeAll = new Subject();
+    constructor(
+        private _coreConfigService: CoreConfigService,
+        private _formBuilder: FormBuilder,
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _authenticationService: AuthenticationService
+    ) {
+        this._unsubscribeAll = new Subject();
 
-    // Configure the layout
-    this._coreConfigService.config = {
-      layout: {
-        navbar: {
-          hidden: true
-        },
-        menu: {
-          hidden: true
-        },
-        footer: {
-          hidden: true
-        },
-        customizer: false,
-        enableLocalStorage: false
-      }
-    };
-    if (this._authenticationService.grpCorpUserValue) {
-      this._router.navigate(['/']);
-    }
-  }
-
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.loginForm.controls;
-  }
-
-  /**
-   * Toggle password
-   */
-  togglePasswordTextType() {
-    this.passwordTextType = !this.passwordTextType;
-  }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    // Login
-    this.loading = true;
-    this._authenticationService
-      .login(this.f.email.value, this.f.password.value)
-      .pipe(first())
-      .subscribe(
-        data => {
-          console.log(this.returnUrl);
-          this._router.navigate([this.returnUrl]);
-        },
-        error => {
-        
-          this.error = "Fallo en la autenticación, vuelva a intentarlo";
-          this.loading = false;
+        // Configure the layout
+        this._coreConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: true
+                },
+                menu: {
+                    hidden: true
+                },
+                footer: {
+                    hidden: true
+                },
+                customizer: false,
+                enableLocalStorage: false
+            }
+        };
+        if (this._authenticationService.grpCorpUserValue) {
+            this._router.navigate(['/']);
         }
-      );
-  }
+    }
 
-  // Lifecycle Hooks
-  // -----------------------------------------------------------------------------------------------------
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.loginForm.controls;
+    }
 
-  /**
-   * On init
-   */
-  ngOnInit(): void {
-    this.loginForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
+    /**
+     * Toggle password
+     */
+    togglePasswordTextType() {
+        this.passwordTextType = !this.passwordTextType;
+    }
 
-    // get return url from route parameters or default to '/'
-    this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
+    onSubmit() {
+        this.submitted = true;
 
-    // Subscribe to config changes
-    this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
-      this.coreConfig = config;
-    });
-  }
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
 
-  /**
-   * On destroy
-   */
-  ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next();
-    this._unsubscribeAll.complete();
-  }
+        // Login
+        this.loading = true;
+        this._authenticationService
+            .login(this.f.email.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    console.log(this.returnUrl);
+                    this._router.navigate([this.returnUrl]);
+                },
+                error => {
+
+                    this.error = 'Fallo en la autenticación, vuelva a intentarlo';
+                    this.loading = false;
+                }
+            );
+    }
+
+    // Lifecycle Hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        this.loginForm = this._formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
+
+        // Subscribe to config changes
+        this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
+            this.coreConfig = config;
+        });
+    }
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
 
 }
--- src/app/main/center/recuperar-pass/recuperar-pass.component.ts
+++ src/app/main/center/recuperar-pass/recuperar-pass.component.ts
@@ -1,106 +1,107 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CoreConfigService } from '@core/services/config.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { RecuperarPassService } from './recuperar-pass.service';
-import { Router } from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CoreConfigService} from '@core/services/config.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {RecuperarPassService} from './recuperar-pass.service';
+import {Router} from '@angular/router';
+
+/*
+* IFIS
+* credit
+* Esta pantalla sirve para ingresar la contraseña por primero vez cuando se creen el usuario
+* Rutas:
+* `${environment.apiUrl}/central/auth/password_reset/`,
+* */
 
 @Component({
-  selector: 'app-recuperar-pass',
-  templateUrl: './recuperar-pass.component.html',
-  styleUrls: ['./recuperar-pass.component.scss']
+    selector: 'app-recuperar-pass',
+    templateUrl: './recuperar-pass.component.html',
+    styleUrls: ['./recuperar-pass.component.scss']
 })
-export class RecuperarPassComponent implements OnInit {
-  // Public
-  public emailVar;
-  public coreConfig: any;
-  public forgotPasswordForm: FormGroup;
-  public submitted = false;
-  public data;
-  public error;
-  // Private
-  private _unsubscribeAll: Subject<any>;
+export class RecuperarPassComponent implements OnInit, OnDestroy {
+    // Public
+    public coreConfig: any;
+    public forgotPasswordForm: FormGroup;
+    public submitted = false;
+    public data;
+    public error;
+    // Private
+    private _unsubscribeAll: Subject<any>;
 
-  /**
-   * Constructor
-   *
-   * @param {CoreConfigService} _coreConfigService
-   * @param {FormBuilder} _formBuilder
-   *
-   */
-  constructor(
-    private _coreConfigService: CoreConfigService,
-    private _formBuilder: FormBuilder,
-    private _router: Router,
-    private _recuperarPassService: RecuperarPassService) {
-    this._unsubscribeAll = new Subject();
+    constructor(
+        private _coreConfigService: CoreConfigService,
+        private _formBuilder: FormBuilder,
+        private _router: Router,
+        private _recuperarPassService: RecuperarPassService) {
+        this._unsubscribeAll = new Subject();
 
-    // Configure the layout
-    this._coreConfigService.config = {
-      layout: {
-        navbar: {
-          hidden: true
-        },
-        footer: {
-          hidden: true
-        },
-        menu: {
-          hidden: true
-        },
-        customizer: false,
-        enableLocalStorage: false
-      }
-    };
-  }
-
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.forgotPasswordForm.controls;
-  }
-  // Lifecycle Hooks
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * On init
-   */
-  ngOnInit(): void {
-    this.forgotPasswordForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]]
-    });
-
-    // Subscribe to config changes
-    this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
-      this.coreConfig = config;
-    });
-  }
-
-  /**
-   * On destroy
-   */
-
-  resetearPassword() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.forgotPasswordForm.invalid) {
-      return;
+        // Configure the layout
+        this._coreConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: true
+                },
+                footer: {
+                    hidden: true
+                },
+                menu: {
+                    hidden: true
+                },
+                customizer: false,
+                enableLocalStorage: false
+            }
+        };
     }
-    this._recuperarPassService.recuperarPassword(this.f.email.value).subscribe((info) => {
-      this.error = null;
-      if(info.status){
-        this._router.navigate(['/grp/login']);
-      }
-    },
-    (error)=>{
-      this.error = "Ocurrió un error al enviar su correo";
-    });
-  }
 
-  ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next();
-    this._unsubscribeAll.complete();
-  }
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.forgotPasswordForm.controls;
+    }
+
+    // Lifecycle Hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        this.forgotPasswordForm = this._formBuilder.group({
+            email: ['', [Validators.required, Validators.email]]
+        });
+
+        // Subscribe to config changes
+        this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
+            this.coreConfig = config;
+        });
+    }
+
+    /**
+     * On destroy
+     */
+
+    resetearPassword() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.forgotPasswordForm.invalid) {
+            return;
+        }
+        this._recuperarPassService.recuperarPassword(this.f.email.value).subscribe((info) => {
+                this.error = null;
+                if (info.status) {
+                    this._router.navigate(['/grp/login']);
+                }
+            },
+            (error) => {
+                this.error = 'Ocurrió un error al enviar su correo';
+            });
+    }
+
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
 
 }
